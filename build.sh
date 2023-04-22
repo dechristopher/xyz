1d5471eab2833c0756fa390daa01b7459175cfc9
Wilko Müller <wimueller@uos.de>
2023-04-22 14:22:08 +0200

    added darwin arm64 to build

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ if [ $# -gt 0 ]; then
   echo "Release: $VERSION"
   echo "Building Darwin (amd64)"
   env GOOS=darwin GOARCH=amd64 go build -o build/xyz-${VERSION}-darwin-amd64
+  echo "Building Darwin (arm64)"
+  env GOOS=darwin GOARCH=arm64 go build -o build/xyz-${VERSION}-darwin-arm64
   echo "Building Linux (amd64)"
   env GOOS=linux GOARCH=amd64 go build -o build/xyz-${VERSION}-linux-amd64
   echo "Building Windows (amd64)"
